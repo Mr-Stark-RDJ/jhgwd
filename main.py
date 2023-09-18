e480214385db29d011e612e8a546e8e7bda75e28
Mr Stark <102538726+Mr-Stark-RDJ@users.noreply.github.com>
2023-09-18 21:08:41 +0530

    Create main.py
    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+api_id = None
+api_hash = None
+
+# Check if API ID and API Hash are already saved
+if os.path.exists("api_credentials.txt"):
+    with open("api_credentials.txt", "r") as file:
+        saved_credentials = file.read().splitlines()
+        if len(saved_credentials) == 2:
+            api_id, api_hash = saved_credentials
+
+# If not saved or if the user wants to change them
+if not api_id or not api_hash or input("Do you want to change API ID and API Hash? (y/n): ").strip().lower() == "y":
+    api_id = input("Please enter your API ID: ").strip()
+    api_hash = input("Please enter your API Hash: ").strip()
+
+    # Save the credentials
+    with open("api_credentials.txt", "w") as file:
+        file.write(api_id + "\n")
+        file.write(api_hash + "\n")
+
+from telethon import TelegramClient, sync, events
+from time import sleep
+from pyrogram import Client, filters
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+import asyncio
+
+app = Client("hexa", api_id, api_hash)
+
+async def main():
+    async with app:
+        while True:
+            await app.send_message("hexamonbot", "/hunt")
+            print("Sending Message ...", end="")
+            await asyncio.sleep(20)
+
+@app.on_message(filters.incoming & filters.chat("hexamonbot"))
+async def echo(client, message):
+    print("... Receiving Message")
+    if not message.text:
+        message.text = message.caption
+    print(message.text)
+    if "found!" in message.text:
+        print("Found Shiny/TM")
+        quit()
+
+app.run(main())
